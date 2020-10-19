12e2ab547d1c6e91b1940b655c2fe0f7a3fbffbe
collinli2022 <collinli2022@gmail.com>
2020-10-19 15:40:30 +0000

    ping command added

--- run.py
+++ run.py
@@ -1,17 +1,13 @@
 import discord
+from discord.ext import commands
 
-client = discord.Client()
+client = discord.Bot(command_prefix = ".")
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith(">hello'):
-        await message.channel.send('Hello Cruel World!')
-
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
 client.run('your token here') # Add token here
