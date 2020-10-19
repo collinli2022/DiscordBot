e28ef02f415a714a5049dd815e56ca7f086ec170
collinli2022 <collinli2022@gmail.com>
2020-10-19 16:10:29 +0000

    command prefix is now >

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-client = discord.Bot(command_prefix = ".")
+client = discord.Bot(command_prefix = ">")
 
 @client.event
 async def on_ready():
@@ -10,4 +10,6 @@ async def on_ready():
 @client.command()
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
+
 client.run('your token here') # Add token here
